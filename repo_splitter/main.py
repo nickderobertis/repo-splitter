6945978ab3673e8c530f9adf121c68918280dc26
Nick DeRobertis <whoopnip@gmail.com>
2019-10-25 19:13:21 -0400

    Add docs and cli for split repo function

--- repo_splitter/main.py
+++ repo_splitter/main.py
@@ -3,6 +3,8 @@ import os
 import time
 from typing import Sequence
 
+import fire
+
 from repo_splitter.git_tools.clone import clone_repo
 from repo_splitter.git_tools.remote import delete_remote
 from repo_splitter.git_tools.history import remove_history_for_files_not_matching, remove_history_for_files_matching
@@ -16,6 +18,21 @@ from repo_splitter.github_tools.query import github_repo_from_clone_url
 def split_repo(repo_source: str, repo_dest: str, new_repo_name: str, keep_files: Sequence[str],
                github_token: str, all_branches: bool = False, include_tags: bool = False,
                remove_files_from_old_repo: bool = True):
+    """
+    Splits an existing Git repository into two repositories by selecting which files should be
+    split into a new one.
+
+    :param repo_source: clone url (remote) or file path (local) of repo that should be split
+    :param repo_dest: folder in which the new repo should be placed
+    :param new_repo_name: name for the new repo
+    :param keep_files: files to be kept in the new repo
+    :param github_token: personal access token for Github
+    :param all_branches: whether to include all branches in the new repo or only the primary (remote)/active (local) one
+    :param include_tags: whether to keep tags from the old repo in the new one
+    :param remove_files_from_old_repo: whether to remove the split files and history from the original repo
+    :return:
+    """
+    # TODO: set up function to work with local only as well (github not required)
     repo_temp_dest = 'testrepo'  # TODO: tempdir
 
     print(f'Creating temporary repo from {repo_source}')
@@ -42,8 +59,8 @@ def split_repo(repo_source: str, repo_dest: str, new_repo_name: str, keep_files:
     shutil.rmtree(repo_temp_dest)
 
     full_repo_dest = os.path.join(repo_dest, new_repo_name)
-    print(f'Cloning {new_repo_name} in permanent spot {full_repo_dest}. Will wait 15s for changes to become available.')
-    time.sleep(15)
+    print(f'Cloning {new_repo_name} in permanent spot {full_repo_dest}. Will wait 5s for changes to become available.')
+    time.sleep(5)
     os.makedirs(full_repo_dest)
     repo = clone_repo(github_repo.clone_url, full_repo_dest, all_branches=all_branches)
 
@@ -70,7 +87,10 @@ def split_repo(repo_source: str, repo_dest: str, new_repo_name: str, keep_files:
     print('Removing temporary directory')
     shutil.rmtree(repo_temp_dest)
 
-    print('Success')
+    print('Success. Please inspect the old repo to make sure nothing that was needed was removed. If it was, '
+          'then you can go to your original local repo and run git push --all --force to reverse it')
 
 
 
+if __name__ == '__main__':
+    fire.Fire(split_repo)
