6241358563309ba395ebb2e6ba1cf1eb1a320d91
Nick DeRobertis <whoopnip@gmail.com>
2019-10-25 19:13:40 -0400

    Use actual temporary directory

--- repo_splitter/main.py
+++ repo_splitter/main.py
@@ -1,5 +1,6 @@
 import shutil
 import os
+import tempfile
 import time
 from typing import Sequence
 
@@ -33,30 +34,28 @@ def split_repo(repo_source: str, repo_dest: str, new_repo_name: str, keep_files:
     :return:
     """
     # TODO: set up function to work with local only as well (github not required)
-    repo_temp_dest = 'testrepo'  # TODO: tempdir
+    with tempfile.TemporaryDirectory() as repo_temp_dest:
+        print(f'Creating temporary repo from {repo_source}')
+        repo = clone_repo(repo_source, repo_temp_dest, all_branches=all_branches)
+        delete_remote(repo)
 
-    print(f'Creating temporary repo from {repo_source}')
-    repo = clone_repo(repo_source, repo_temp_dest, all_branches=all_branches)
-    delete_remote(repo)
+        print('Removing unwanted history from temporary repo')
+        remove_history_for_files_not_matching(repo, keep_files)
 
-    print('Removing unwanted history from temporary repo')
-    remove_history_for_files_not_matching(repo, keep_files)
+        print(f'Creating Github repo {new_repo_name}')
+        github_repo = create_repo(github_token, new_repo_name)
 
-    print(f'Creating Github repo {new_repo_name}')
-    github_repo = create_repo(github_token, new_repo_name)
+        print(f'Pushing local temporary repo to github repo {new_repo_name}')
+        connect_local_repo_to_github_repo(repo, github_repo, github_token)
+        if all_branches:
+            push_all_branches(repo)
+        else:
+            push_active_branch(repo)
 
-    print(f'Pushing local temporary repo to github repo {new_repo_name}')
-    connect_local_repo_to_github_repo(repo, github_repo, github_token)
-    if all_branches:
-        push_all_branches(repo)
-    else:
-        push_active_branch(repo)
+        if include_tags:
+            push_tags(repo)
 
-    if include_tags:
-        push_tags(repo)
-
-    print('Removing temporary directory')
-    shutil.rmtree(repo_temp_dest)
+        print('Removing temporary directory')
 
     full_repo_dest = os.path.join(repo_dest, new_repo_name)
     print(f'Cloning {new_repo_name} in permanent spot {full_repo_dest}. Will wait 5s for changes to become available.')
@@ -69,26 +68,27 @@ def split_repo(repo_source: str, repo_dest: str, new_repo_name: str, keep_files:
         return
 
     print(f'Cleaning up what was split off in the old repo')
-    os.makedirs(repo_temp_dest)
-    print(f'Cloning {repo_source} into temporary directory')
-    repo = clone_repo(repo_source, repo_temp_dest, all_branches=True)
-    if is_remote_url(repo_source):
-        # If remote, need to add authentication into the remote
-        github_repo = github_repo_from_clone_url(repo_source, github_token)
-        delete_remote(repo)
-        connect_local_repo_to_github_repo(repo, github_repo, github_token)
+    with tempfile.TemporaryDirectory() as repo_temp_dest:
+        print(f'Cloning {repo_source} into temporary directory')
+        repo = clone_repo(repo_source, repo_temp_dest, all_branches=True)
+        if is_remote_url(repo_source):
+            # If remote, need to add authentication into the remote
+            github_repo = github_repo_from_clone_url(repo_source, github_token)
+            delete_remote(repo)
+            connect_local_repo_to_github_repo(repo, github_repo, github_token)
 
-    print(f'Removing history in the original repo for files which were split off')
-    remove_history_for_files_matching(repo, keep_files)
+        print(f'Removing history in the original repo for files which were split off')
+        remove_history_for_files_matching(repo, keep_files)
 
-    print('Pushing to remote for the original repo')
-    push_all_force(repo)
+        print('Pushing to remote for the original repo')
+        push_all_force(repo)
 
-    print('Removing temporary directory')
-    shutil.rmtree(repo_temp_dest)
+        print('Removing temporary directory')
 
-    print('Success. Please inspect the old repo to make sure nothing that was needed was removed. If it was, '
+    print('Success. Please inspect the old repo to make sure nothing that was needed was removed.')
+    print('If there is an issue, '
           'then you can go to your original local repo and run git push --all --force to reverse it')
+
 
 
 
