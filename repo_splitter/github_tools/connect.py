e61792d870c487a4a76749846156c78026d5b223
Nick DeRobertis <whoopnip@gmail.com>
2019-10-25 16:56:56 -0400

    Add authentication to remote url so it is not required to authenticate to push

--- repo_splitter/github_tools/connect.py
+++ repo_splitter/github_tools/connect.py
@@ -2,6 +2,12 @@ from git import Repo, Remote
 from github.Repository import Repository
 
 
-def connect_local_repo_to_github_repo(local_repo: Repo, github_repo: Repository) -> Remote:
-    origin = local_repo.create_remote('origin', github_repo.clone_url)
+def connect_local_repo_to_github_repo(local_repo: Repo, github_repo: Repository, username: str,
+                                      token: str) -> Remote:
+    url = _github_authenticated_url(github_repo, username, token)
+    origin = local_repo.create_remote('origin', url)
     return origin
+
+
+def _github_authenticated_url(github_repo: Repository, username: str, token: str) -> str:
+    return github_repo.clone_url.replace('github.com', f'{username}:{token}@github.com')
