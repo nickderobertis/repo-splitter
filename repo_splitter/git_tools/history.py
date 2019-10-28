a458694277d52746704d9de959774edbb3a19b56
Nick DeRobertis <whoopnip@gmail.com>
2019-10-28 09:51:58 -0400

    Add better debugging output for index filter branch

--- repo_splitter/git_tools/history.py
+++ repo_splitter/git_tools/history.py
@@ -1,9 +1,13 @@
 from typing import Sequence
+import re
 
-from git import Repo
+from git import Repo, GitCommandError
 
 from repo_splitter.git_tools.files.unwanted import get_unwanted_files_from_repo
 from repo_splitter.git_tools.files.wanted import get_desired_files_from_patterns
+
+
+START_COMMIT_PATTERN = r"Start of output for commit: [\d\w]+"
 
 
 def remove_history_for_files_not_matching(repo: Repo, file_patterns: Sequence[str]):
@@ -21,4 +25,60 @@ def _remove_history_except_for_files(repo: Repo, files: Sequence[str]):
     wanted_files_str = '|'.join(starts_with_wanted_files)
     index_filter_cmd = f'git ls-files | grep -vE "{wanted_files_str}" | xargs git rm -rf --cached --ignore-unmatch'
 
-    repo.git.filter_branch('--prune-empty', '--index-filter', index_filter_cmd, '--', '--all')
+    index_filter_branch(repo, index_filter_cmd)
+
+
+def index_filter_branch(repo: Repo, index_filter_cmd: str):
+    # Add debug info
+    index_filter_cmd = f"""
+    set -x;
+    echo "\n\n\nStart of output for commit: $GIT_COMMIT";
+    echo $(printenv);
+    {index_filter_cmd};
+    EXIT_CODE=$?;
+    echo "End of output for commit: $GIT_COMMIT \n\n\n";
+    (exit $EXIT_CODE);
+    """
+    _filter_branch(repo, '--prune-empty', '--index-filter', index_filter_cmd, '--', '--all')
+
+
+def _filter_branch(repo: Repo, *args, **kwargs):
+    try:
+        output = repo.git.filter_branch(*args, **kwargs)
+        return output
+    except GitCommandError as e:
+        exc = GitFilterBranchException.from_git_command_error(e)
+        raise exc from None
+
+
+class GitFilterBranchException(Exception):
+
+    def __init__(self, status: int, stdout: str, stderr: str, *args, **kwargs):
+        self.status = status
+        self.stdout = stdout
+        self.stderr = stderr
+
+    @classmethod
+    def from_git_command_error(cls, git_error: GitCommandError):
+        return cls(
+            git_error.status,
+            git_error.stdout,
+            git_error.stderr,
+        )
+
+    @staticmethod
+    def _extract_from_last_commit(output: str) -> str:
+        last_commit_start = [match for match in re.finditer(START_COMMIT_PATTERN, output)][-1].start(0)
+        return output[last_commit_start:]
+
+    def __str__(self):
+        message = f"""
+        Stdout:
+        {GitFilterBranchException._extract_from_last_commit(self.stdout)}
+
+        Stderr:
+        {GitFilterBranchException._extract_from_last_commit(self.stderr)}
+
+        Note: Full stdout and stderr available in exception exc.stdout and exc.stderr
+        """
+        return message
